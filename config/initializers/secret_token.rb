578c867c99f35763a582151fd05a901deaaac9b6
Ben Gruber <blgruber@gmail.com>
2013-10-01 15:04:07 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whipplee::Application.config.secret_token = '39b9e2171555e14e2062d48fcc637b05dc1a542347032f5c84ec4bf24bde56bde51e589ead874a2e2d3c4f50d6252e7366b7dc2c25dd2f158e8d3b72fd377460'
