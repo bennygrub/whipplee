ff25b9051621391aca09e6f2091da0f765381a05
Ben Gruber <blgruber@gmail.com>
2013-10-18 18:24:02 -0400

    desc ordering on friend feeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     @following = @user.following
     @autofollowers = @user.following_ids
     @following_ids = @following.map {|follow| follow.id }
-    @friends_favorites = Favorite.where("user_id IN (?)", @following_ids)
+    @friends_favorites = Favorite.where("user_id IN (?)", @following_ids).order("created_at DESC")
     @friends_favorite_posts = @friends_favorites.map {|favorite| Post.find(favorite.post_id)}
     @my_favorite_posts = @user.favorites.map {|favorite| Post.find(favorite.post_id)}
   end
